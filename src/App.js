5a545536196a06098e3771a46e76843c64f2be56
Dada Oluwakemi <dadaoluwakemi97@gmail.com>
2021-02-26 13:27:41 +0100

    Iniial Commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Alert from './components/layout/Alert';
+import Navbar from './components/layout/Navbar';
+import About from './components/pages/About';
+import User from './components/users/User';
+import Home from './components/pages/Home';
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+import NotFound from './components/pages/NotFound';
+import './App.css';
+
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
+
+export default App;
---
+++ src/components/repos/Repos.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+import RepoItem from './RepoItem';
+import GithubContext from '../../context/github/githubContext';
+
+const Repos = () => {
+  const githubContext = useContext(GithubContext);
+  return githubContext.repos.map((repo) => (
+    <RepoItem repo={repo} key={repo.id} />
+  ));
+};
+
+export default Repos;
